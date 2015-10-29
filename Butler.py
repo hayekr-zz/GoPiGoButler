e6a0a5ea43d77c21384958a5d4f2483c25614dd8
Robert Hayek <hayekr@gilmour.org>
2015-10-29 08:52:14 -0400

    added Stop and return Servo Position Methods

--- Butler.py
+++ Butler.py
@@ -1,11 +1,28 @@
-#from gopigo import *
+from gopigo import *
+import time
 
 __author__ = "Robert Hayek and Keith Neyman "
 
 class Pigo:
 
+    isMoving = False
+    servoPos = 90
+    currentPos =
+
     def __init__(self):
         print "WORKING"
 
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "Error, Try Again"
+
+
+    def currentServoPos:
+
+
 butler = Pigo()
+
+
 
