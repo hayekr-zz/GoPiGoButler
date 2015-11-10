dab86ba08d4bc1f9542e3a19b5c0397f66d9a255
Robert Hayek <Robert Hayek>
2015-11-10 08:52:58 -0500

    print and new elif in keepGoing method

--- Butler.py
+++ Butler.py
@@ -3,7 +3,15 @@ __author__ = 'Robert Hayek and Keith Neyman'
 from gopigo import *
 import time
 
+#Global Variable that says how far butler can get to a wall
 STOP_DIST = 50
+
+#print variables
+error = "ERROR"
+volt = "HAZARDOUS VOLTAGE"
+on = "BUTLERPI IS NOW ON"
+stop = "STOPPING"
+move = "MOVING"
 
 class Butler:
 
@@ -13,23 +21,26 @@ class Butler:
     status = {'isMoving' : False, 'servo' : 90, 'leftSpeed' : 175, 'rightSpeed' : 175, "distance" : 100}
 
     def __init__(self):
-        print "ROBOT IS NOW ON"
-        self.Butler = "Butler"
+        print on
+        self.Butler = "Butler" #define self
         self.status['distance'] = us_dist(15) #update distance  with the current distance through ultrasonic sensor in (mm)
     def stop(self):
         self.status["isMoving"] = False
-        print "STOPPING" #debugging message (delete later after)
+        print stop #debugging message (delete later after)
         for x in range(3):
             stop()
 
     def fwd(self):
         self.isMoving = True
-        print "MOVING" #debugging message (delete later)
+        print move #debugging message (delete later)
         for x in range(3):
             fwd()
-
+    #Check if conditions are safe for ButlerPi to continue
     def keepGoing(selfself):
         if self.status['distance'] < STOP_DIST:
+            return False
+        elif volt() > 14 or volt() < 6:
+            print volt
             return False
         else:
             return True
