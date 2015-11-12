be411ffc8ebec9a09a0d07721f88f6dc11432bed
Robert Hayek <hayekr@gilmour.org>
2015-11-12 12:11:35 -0500

    Added comments

--- allstop.py
+++ allstop.py
@@ -3,7 +3,7 @@ from gopigo import *
 __author__ = 'Robert Hayek and Keith Neyman'
 
 
-for x in range(6):
-    stop()
-    stop()
+for x in range(6):##########
+    stop()        ########Turns off all motors when file is started
+    stop()        ##########
 disable_servo()
