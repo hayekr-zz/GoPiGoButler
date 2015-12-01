b62ea35e60216d092e0bb0dfa2c05f92efddb525
Robert Hayek <hayekr@gilmour.org>
2015-12-01 09:37:41 -0500

    minor changes

---
+++ obst_avoidance.py
@@ -0,0 +1,12 @@
+#GOPIGO API http://www.dexterindustries.com/GoPiGo/programming/python-programming-for-the-raspberry-pi-gopigo/
+
+#imports
+from gopigo import *
+import time
+
+__author__ = 'Robert Hayek and Keith Neyman'
+
+# Global Variable that says how far butler can get to a wall
+STOP_DIST = 15
+
+class Pigo:
