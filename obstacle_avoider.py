bc72c7824ecf098fc0a0c7af0c03a77f5fa96ed6
Robert Hayek <hayekr@gilmour.org>
2015-12-01 09:21:56 -0500

    added obst avoider file

---
+++ obstacle_avoider.py
@@ -0,0 +1,7 @@
+#GOPIGO API http://www.dexterindustries.com/GoPiGo/programming/python-programming-for-the-raspberry-pi-gopigo/
+
+#imports
+from gopigo import *
+import time
+
+__author__ = 'Robert Hayek and Keith Neyman'
