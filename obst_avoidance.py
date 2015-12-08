cb6151e4c7f443fdf6704cfbca88e64ac4f0b1b1
Robert Hayek <hayekr@gilmour.org>
2015-12-08 09:00:55 -0500

    BUG FIX

--- obst_avoidance.py
+++ obst_avoidance.py
@@ -11,7 +11,7 @@ servo(80)
 class Pigo:
     sweep = [None] * 160  #the list to hold scanning data
     MIN_DIST = 50  #distance used when plotting a clear direction... longer so we're planning farther ahead
-    status = {'isMoving' : False, 'servo': 90, 'leftSpeed' : 175, 'rightSpeed' : 175, "distance" : 100}
+    status = {'isMoving': False, 'servo': 90, 'leftSpeed': 175, 'rightSpeed': 175, "distance": 100}
     def __init__(self):
         print "NOW RUNNING OBSTACLE AVOIDER"
 
@@ -19,14 +19,10 @@ class Pigo:
         for angle in range(15, 150, 5):
             servo(angle)
             time.sleep(.10)
-            self.sweep['angle'] = us_dist(15)
+            self.sweep[angle] = us_dist(15)
             print self.sweep[angle]
 
-    def checkDistance(self):
-        self.status['distance'] = us_dist(15)
-        print "CHECKING DISTANCE..." + "SOMETHING IS " + str(self.status['distance']) + "mm away"
-
-    def findaPath(self):
+    def findPath(self):
         for angle in self.sweep:
             counter = 0
             if self.sweep[angle] > MIN_DIST:
@@ -37,19 +33,22 @@ class Pigo:
                 return True
         return False
 
+    def turnAround(self):
+        right_rot()
+        self.sleep(.10)
 
 butler = Pigo()
 
 ''' while  True:
     if butler.servoScan():
         butler.safeDrive()
-    if butler.findaPath():
+    if butler.findPath():
         butler.turnTo(butler.findAngle())
     else:
         butler.turnAround()
 butler.stop()
 '''
 butler.servoSweep()
-print butler.findaPath()
+print butler.findPath()
 
 
